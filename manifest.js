911c9d3128667bafb882828387cccbd47b88ad3f
SANECA MACK <SANECATV@GMAIL.COM>
2026-06-09 05:33:19 -0400

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,14 @@
+{
+  "name": "STV Player Fit",
+  "short_name": "STV FIT",
+  "description": "STV Radiomeltdown Red player",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#051a0a",
+  "theme_color": "#051a0a",
+  "orientation": "portrait",
+  "icons": [
+    { "src": "icon-192.png", "sizes": "192x192", "type": "image/png" },
+    { "src": "icon-512.png", "sizes": "512x512", "type": "image/png" }
+  ]
+}
